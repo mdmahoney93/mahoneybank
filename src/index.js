57e88beb037c44247c59dabd1f378e1df39908a9
mdmahoney93 <mdmahoney93@Gmail.com>
2021-11-29 13:30:25 -0600

    changes3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+function Spa() {
+  return (
+    <HashRouter>
+      <NavBar />
+      <UserContext.Provider
+        value={{
+          users: [
+            {
+              name: "abel",
+              email: "abel@mit.edu",
+              password: "secret",
+              balance: 100,
+            },
+          ],
+        }}
+      >
+        <div className="container" style={{ padding: "20px" }}>
+          <Route path="/" exact component={Home} />
+          <Route path="/CreateAccount/" component={CreateAccount} />
+          <Route path="/login/" component={Login} />
+          <Route path="/deposit/" component={Deposit} />
+          <Route path="/withdraw/" component={Withdraw} />
+          <Route path="/balance/" component={Balance} />
+          <Route path="/alldata/" component={AllData} />
+        </div>
+      </UserContext.Provider>
+    </HashRouter>
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Spa />, document.getElementById("root"));
